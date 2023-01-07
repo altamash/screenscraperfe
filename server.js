e9061656587743a7d17feae872af85b62c5994e4
Mohammad Altamash <ultamesh@gmail.com>
2023-01-08 01:30:05 +0500

    Update

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get("/*", function (req, res) {
   res.sendFile("index.html", { root: "dist/screenscraper" });
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
 
-console.log(`Running on port ${process.env.PORT || 8080}`);
+console.log(`Running on port ${process.env.PORT || 4200}`);
